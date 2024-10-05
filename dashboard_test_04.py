ed010001dfa34c1dd4bb30c6c785cb2182bad7e8
이진녕 <jinyoung.lee@cjconnect.io>
2024-10-05 15:26:25 +0900

    03 04 도 업로드

---
+++ dashboard_test_04.py
@@ -0,0 +1,32 @@
+from common import *
+
+def test_01():
+    assert 1
+        
+def test_02():
+    assert 1
+            
+def test_03():
+    assert 1
+        
+def test_04():
+    assert 1
+        
+def test_05():
+    assert 1
+        
+def test_06():
+    assert 1
+            
+def test_07():
+    assert 1
+        
+def test_08():
+    assert 1
+            
+def test_09():
+    assert 1
+        
+def test_10():
+    assert 1
+        
